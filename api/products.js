f640eb84fb4e12896a64b321425fce6b72165903
Rushikesh Palodkar <rpalodkar15@gmail.com>
2025-08-12 13:43:24 -0400

    Fix Vercel API exports and Windows build script

--- api/products.js
+++ api/products.js
@@ -74,7 +74,7 @@ async function writeProducts(products) {
     }
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
     // Enable CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
